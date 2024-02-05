d529413b47fef0656c5b464af97fb8f66b2804f4
Darrin-Lin <darrin.de88@gmail.com>
2024-02-05 14:46:11 +0800

    fix nav and footer

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -5,17 +5,34 @@ const Footer = () => {
     <footer>
       <div class="contact">
         <h2 class="contact_title contact_text">聯絡方式</h2>
-        <p class="contact_content contact_text">信箱：camp@csie.cool<br></br>
+        <p class="contact_content contact_text">
+          信箱：camp@csie.cool<br></br>
           連絡電話：0978-935-279<br></br>
-          (張建堯 總籌)</p>
+          (張建堯 總籌)
+        </p>
       </div>
       <div class="social_media">
-        <a href="https://www.facebook.com/ntnucsiecamp" target="_blank"><img src="./image/fb.png"
-          alt="facebook" class="facebook_icon social_media_icon"></img></a>
-        <a href="https://www.instagram.com/ntnucsiecamp/" target="_blank"><img src="image/ig.png"
-          alt="instagram" class="instagram_icon social_media_icon"></img></a>
-        <a href="http://m.me/ntnucsiecamp" target="_blank"><img src="./image/messenger.png" alt="messenger"
-          class="messenger_icon social_media_icon"></img></a>
+        <a href="https://www.facebook.com/ntnucsiecamp" target="_blank">
+          <img
+            src={require("./image/fb.png")}
+            alt="facebook"
+            class="facebook_icon social_media_icon"
+          ></img>
+        </a>
+        <a href="https://www.instagram.com/ntnucsiecamp/" target="_blank">
+          <img
+            src={require("./image/ig.png")}
+            alt="instagram"
+            class="instagram_icon social_media_icon"
+          ></img>
+        </a>
+        <a href="http://m.me/ntnucsiecamp" target="_blank">
+          <img
+            src={require("./image/messenger.png")}
+            alt="messenger"
+            class="messenger_icon social_media_icon"
+          ></img>
+        </a>
       </div>
     </footer>
   );
--- my-app/src/Nav.js
+++ my-app/src/Nav.js
@@ -33,13 +33,39 @@ const Nav = () => {
   return (
     <nav class="banner banner_background">
       <ul class="nav_ul">
-        <a href="#" class="home_link"><img src="./image/logo_circle.png" alt="logo" class="banner_logo"></img></a>
-        <li class="banner_button"><a href="login" class="login_button ">立即報名</a></li>
-        <li class="banner_button"><a href="#review" class="review_button">歷屆回顧</a></li>
-        <li class="banner_button"><a href="#traffic" class="traffic_button">交通資訊</a></li>
-        <li class="banner_button"><a href="#faq" class="faq_button">常見問題</a></li>
-        <li class="banner_button"><a href="#info" class="info_button">報名資訊</a></li>
-        <li class="banner_button"><a href="#about" class="about_button">營隊介紹</a></li>
+        <a href="#" class="home_link">
+          <img src={require("./image/logo_circle.png")} alt="logo" class="banner_logo"></img>
+        </a>
+        <li class="banner_button">
+          <a href="login" class="login_button ">
+            立即報名
+          </a>
+        </li>
+        <li class="banner_button">
+          <a href="#review" class="review_button">
+            歷屆回顧
+          </a>
+        </li>
+        <li class="banner_button">
+          <a href="#traffic" class="traffic_button">
+            交通資訊
+          </a>
+        </li>
+        <li class="banner_button">
+          <a href="#faq" class="faq_button">
+            常見問題
+          </a>
+        </li>
+        <li class="banner_button">
+          <a href="#info" class="info_button">
+            報名資訊
+          </a>
+        </li>
+        <li class="banner_button">
+          <a href="#about" class="about_button">
+            營隊介紹
+          </a>
+        </li>
       </ul>
     </nav>
   );
