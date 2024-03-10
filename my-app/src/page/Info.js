078feb2dab210ddd6f971a90c95602f37066ce1c
Darrin-Lin <darrin.de88@gmail.com>
2024-03-10 16:04:05 +0800

    edit regist in info

--- my-app/src/page/Info.js
+++ my-app/src/page/Info.js
@@ -2,26 +2,26 @@ import React from "react";
 
 export const Info = () => {
   function attend_click() {
-    window.location.replace("https://www.google.com.tw/");
+    window.location.replace("/login");
   }
   return (
     <div class="attend_info_">
       <div class="lesson">
-        <h2 >日期</h2>
+        <h2>日期</h2>
         <img src={require("../image/dateicon.png")} alt="date_icon" class="imageinfo"></img>
       </div>
 
       <div class="lesson">
-        <h2 >地點</h2>
+        <h2>地點</h2>
         <img src={require("../image/school_icon.png")} alt="school_icon" class="imageinfo"></img>
       </div>
       <div class="lesson">
-        <h2 >報名時間</h2>
+        <h2>報名時間</h2>
         <img src={require("../image/clockicon.png")} alt="clock_icon" class="imageinfo"></img>
       </div>
 
       <div class="lesson">
-        <h2 >個人報名</h2>
+        <h2>個人報名</h2>
         <img src={require("../image/soloicon.png")} alt="solo_icon" class="imageinfo"></img>
       </div>
       <div class="lesson">
@@ -30,7 +30,7 @@ export const Info = () => {
       </div>
 
       <button type="button" class="attend_button" onClick={attend_click}>
-        報名營隊 
+        報名營隊
       </button>
     </div>
   );
