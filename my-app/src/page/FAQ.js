8054bacd584d67d3d4514390f544f9c0836b962c
whyhugo <hugo.tw.wang@gmail.com>
2024-03-13 22:46:11 +0800

    feat: update FAQ page text transform

--- my-app/src/page/FAQ.js
+++ my-app/src/page/FAQ.js
@@ -1,26 +1,15 @@
 import React from "react";
 
 export const FAQ = () => {
-  document.addEventListener("DOMContentLoaded", function () {
-    var gridItems = document.querySelectorAll(".grid-item");
-
-    gridItems.forEach(function (item) {
-      item.addEventListener("mouseenter", function () {
-        this.classList.add("hovered");
-      });
-
-      item.addEventListener("mouseleave", function () {
-        this.classList.remove("hovered");
-      });
-    });
-  });
   return (
     <div id="page_faq">
+      {/*
       <div class="comment">
         <h3>
           <center>點擊問題方塊來為你解惑吧！</center>
         </h3>
       </div>
+      */}
       <div class="grid-container">
         <div class="grid-item wide">
           <div class="question">
@@ -34,39 +23,50 @@ export const FAQ = () => {
           </div>
         </div>
         <div class="grid-item narrow">
-          <h2>上課內容是什麼？</h2>
-          <p>
-            回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆
-            1 回覆 1 回覆 1 回覆 1{" "}
-          </p>
+          <div class="question">
+            <h2>上課內容是什麼？</h2>
+          </div>
+          <div class="response">
+            <p>回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 </p>
+          </div>
         </div>
         <div class="grid-item narrow">
-          <h2>要如何報名？</h2>
-          <p>
-            回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆
-            1 回覆 1 回覆 1 回覆 1{" "}
-          </p>
+          <div class="question">
+            <h2>要如何報名？</h2>
+          </div>
+          <div class="response">
+            <p>回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 </p>
+          </div>
         </div>
         <div class="grid-item wide s">
-          <h2>報名的資格有限制嗎？</h2>
-          <p>
-            回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆
-            1 回覆 1 回覆 1 回覆 1{" "}
-          </p>
+          <div class="question">
+            <h2>報名的資格有限制嗎？</h2>
+          </div>
+          <div class="response">
+            <p>
+              回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1
+              回覆 1 回覆 1 回覆 1 回覆 1{" "}
+            </p>
+          </div>
         </div>
         <div class="grid-item wide">
-          <h2>從哪裡獲得最新資訊？</h2>
-          <p>
-            回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆
-            1 回覆 1 回覆 1 回覆 1{" "}
-          </p>
+          <div class="question">
+            <h2>從哪裡獲得最新資訊？</h2>
+          </div>
+          <div class="response">
+            <p>
+              回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1
+              回覆 1 回覆 1 回覆 1 回覆 1{" "}
+            </p>
+          </div>
         </div>
         <div class="grid-item narrow">
-          <h2>退費機制？</h2>
-          <p>
-            回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆
-            1 回覆 1 回覆 1 回覆 1{" "}
-          </p>
+          <div class="question">
+            <h2>退費機制？</h2>
+          </div>
+          <div class="response">
+            <p>回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 回覆 1 </p>
+          </div>
         </div>
       </div>
     </div>
