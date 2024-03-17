4e0c127505c9246f5bc9f9d0ee3984f1663725fb
whyhugo <hugo.tw.wang@gmail.com>
2024-03-18 03:44:34 +0800

    fix: about page index bug

--- my-app/src/page/Home.js
+++ my-app/src/page/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import camp_introduction_img from "../image/draw_icon/camp_introduction.png";
 import registration_information_img from "../image/draw_icon/registration_information.png";
 import faq_img from "../image/draw_icon/faq.png";
 import traffic_information_img from "../image/draw_icon/traffic_information.png";
@@ -12,10 +13,11 @@ import Review from "./Review";
 
 const Home = () => {
   const [descriptions, setDescriptions] = useState({
-    image1: { visible: true, content: <Info /> },
-    image2: { visible: true, content: <FAQ /> },
-    image3: { visible: true, content: <Traffic /> },
-    image4: { visible: true, content: <Review /> },
+    image1: { visible: true, content: <About /> },
+    image2: { visible: true, content: <Info /> },
+    image3: { visible: true, content: <FAQ /> },
+    image4: { visible: true, content: <Traffic /> },
+    image5: { visible: true, content: <Review /> },
   });
 
   const toggleDescription = (imageId) => {
@@ -35,19 +37,10 @@ const Home = () => {
       </header>
       <div className="container">
         <div>
-          <div>
-            <section>
-              <p>
-                <About />
-              </p>
-              <p>&nbsp;</p>
-            </section>
-          </div>
-
           <div className="col-6 col-12-medium">
             <section>
               <ul className="big-image-list" style={{ listStyleType: "none" }}>
-                {[1, 2, 3, 4].map((index) => (
+                {[1, 2, 3, 4, 5].map((index) => (
                   <li key={index}>
                     {index > 1 && <div style={{ height: "100%" }}></div>}
                     <div style={{ textAlign: index % 2 === 0 ? "right" : "left" }}>
@@ -55,20 +48,23 @@ const Home = () => {
                         style={{ textAlign: index % 2 === 0 ? "right" : "left" }}
                         class="home_titles"
                       >
-                        {index === 1 && <font id="info">報名資訊</font>}
-                        {index === 2 && <font id="faq">FAQ</font>}
-                        {index === 3 && <font id="traffic">交通資訊</font>}
-                        {index === 4 && <font id="review">歷屆回顧</font>}
+                        {index === 1 && <font id="about">營隊資訊</font>}
+                        {index === 2 && <font id="info">報名資訊</font>}
+                        {index === 3 && <font id="faq">FAQ</font>}
+                        {index === 4 && <font id="traffic">交通資訊</font>}
+                        {index === 5 && <font id="review">歷屆回顧</font>}
                       </h2>
                       <hr />
                       <a href="#" onClick={() => toggleDescription(`image${index}`)}>
                         <img
                           src={
                             index === 1
-                              ? registration_information_img
+                              ? camp_introduction_img
                               : index === 2
-                              ? faq_img
+                              ? registration_information_img
                               : index === 3
+                              ? faq_img
+                              : index === 4
                               ? traffic_information_img
                               : review_img
                           }
