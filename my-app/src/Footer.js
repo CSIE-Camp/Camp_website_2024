5a2a17c2f54c92b1975c016b888bb4845a42a585
Darrin-Lin <darrin.de88@gmail.com>
2024-04-02 17:56:23 +0800

    modify: hide old wesite.

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -3,14 +3,15 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <div class="before">
+      {/* <div class="before">
         <h3 class="before_title">歷年網站</h3>
         <a href="/old/2023">2023</a>
-      </div>
+      </div> */}
       <div class="contact">
         <h2 class="contact_title contact_text">聯絡方式</h2>
         <p class="contact_content contact_text">
-          信箱：<a href="mailto:ntnucsiecamp2024@gmail.com">ntnucsiecamp2024@gmail.com</a><br></br>
+          信箱：<a href="mailto:ntnucsiecamp2024@gmail.com">ntnucsiecamp2024@gmail.com</a>
+          <br></br>
           連絡電話：0920-292-613<br></br>
           （鄭聿喬 總籌）
         </p>
