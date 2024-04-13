11cb3d2a10b57250576d84fc129039ac808657e2
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-04-13 19:13:18 +0800

    Update Home.js

--- my-app/src/page/Home.js
+++ my-app/src/page/Home.js
@@ -12,7 +12,8 @@ import Traffic from "./Traffic";
 import Review from "./Review";
 
 const Home = () => {
-  if(window.location.href != "https://camp.csie.cool/"){window.location.href = "https://camp.csie.cool/"}
+  if(window.location.href ==== "https://camp.csie.cool/"){}
+  else {window.location.href = "https://camp.csie.cool/"}
   const [descriptions, setDescriptions] = useState({
     image1: { visible: true, content: <About /> },
     image2: { visible: true, content: <Info /> },
