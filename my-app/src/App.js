68b7cd2b8cac8b14178baf6fe10eeee85ee9e272
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-04-13 19:29:35 +0800

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 const App = () => {
   return (
     <div className="App">
-    <script>if(window.location.href != "https://camp.csie.cool/"){window.location.href = "https://camp.csie.cool/"}</script>
+    
       <div id="bg">
         <div id="container">
           <div id="top"></div>
